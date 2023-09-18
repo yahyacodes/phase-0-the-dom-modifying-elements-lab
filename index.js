4ee1444158bf3c5c89a882d89115a1619693eaa3
yahyacodes <yahya.codin@gmail.com>
2023-09-18 22:37:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const main = document.querySelector("main#main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+newHeader.textContent = "YOUR-NAME is the champion";
